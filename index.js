23b712d3a6765fde369b72e43d5d82f5aab9777b
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-05-12 11:49:47 +0100

    code edit

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ let menuBar = document.querySelector('.menu');
 let menuContent = document.querySelector('.desktop_menu')
 let introSection = document.querySelector('.intro')
 let sec1 = document.querySelector('.sec1')
-
+let links = document.querySelectorAll('.link')
 
 
 //  menu bar
 menuBar.addEventListener("click", ()=> {
   menuContent.classList.toggle("active");
-  menuContent.style.width = "100%"
+  menuContent.style.width = "70%"
   menuContent.style.position = 'fixed'
   introSection.classList.toggle("active")
-  // sec1.classList.toggle('active')
 })
+
+// Links
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuContent.classList.remove("active");
+  });
+});
 
 
